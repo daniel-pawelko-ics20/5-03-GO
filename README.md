9640781c538bddb5751eb1a1a9e1fb33ff8b331f
danielpawelkoic <>
2021-05-26 02:04:21 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 5-03-GO
+[![GitHub's Super Linter](https://github.com/daniel-pawelko-ics20/5-03-GO/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/daniel-pawelko-ics20/5-03-GO/actions)
---
+++ main.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 Daniel Pawelko All rights reserved
+//
+// Created by: Daniel Pawelko
+// Created on: May 2021
+// This file contains GO program that returns what movie a user can watch alone
+
+package main
+
+import (
+	"fmt"
+)
+
+// This main function will ask user for age and return what the user can watch
+func main() {
+	// Defining variables
+	var age int
+
+	fmt.Println("What movie can you watch?")
+	fmt.Println()
+
+	// User Input
+	fmt.Print("Age: ")
+	fmt.Scanln(&age)
+	fmt.Println()
+
+	// Return what user can watch
+	if age >= 17 {
+		fmt.Println("You can see an R movie alone.")
+	} else if age >= 13 {
+		fmt.Println("You can see a PG-13 movie alone.")
+	} else if age >= 5 {
+		fmt.Println("You can see a G or PG movie alone.")
+	} else {
+		fmt.Println("Uh. You're too young for most things.")
+	}
+}
